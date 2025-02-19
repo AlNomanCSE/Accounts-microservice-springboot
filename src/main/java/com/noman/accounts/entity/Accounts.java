0bd28796559d8de4f9b23ae831c52106c06e47f4
AlNomanCSE <abdullahalnomancse@gmail.com>
2025-02-19 23:38:57 +0600

    Entity and Repository added

---
+++ src/main/java/com/noman/accounts/entity/Accounts.java
@@ -0,0 +1,22 @@
+package com.noman.accounts.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Accounts extends BaseEntity {
+
+    private Long customerId;
+
+    @Id
+    private String accountNumber;
+    private String email;
+    private String mobileNumber;
+}
